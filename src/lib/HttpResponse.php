a8028ff2cf99a598076c6b67041972cbee089f08
axios <axioscros@aliyun.com>
2018-07-24 14:58:13 +0800

    增加获取data方法，支持获取数组中的某一元素或其自元素

--- src/lib/HttpResponse.php
+++ src/lib/HttpResponse.php
@@ -11,50 +11,103 @@ namespace api\tool\lib;
 
 class HttpResponse
 {
-    private $header;
+    private $header = [];
 
     private $body;
 
     private $status;
+    
+    private $data;
 
+    /**
+     * @param $header
+     * @return $this
+     */
     public function setHeader($header)
     {
         $this->header = $header;
         return $this;
     }
 
+    /**
+     * @return array
+     */
     public function getHeader()
     {
         return $this->header;
     }
 
+    /**
+     * @param $status
+     * @return $this
+     */
     public function setStatus($status)
     {
         $this->status = $status;
         return $this;
     }
 
+    /**
+     * @return mixed
+     */
     public function getStatus()
     {
         return $this->status;
     }
 
+    /**
+     * @param $body
+     * @return $this
+     */
     public function setBody($body)
     {
         $this->body = $body;
         return $this;
     }
 
+    /**
+     * @param string $key
+     * @return array
+     */
+    public function getData($key = null){
+        $data = $this->getContent();
+
+        if(is_null($key)){
+            return $data;
+        }
+
+        if(false !== strpos($key,'.')){
+            $keys = explode('.',$key);
+            $tmp = $data;
+            foreach ($keys as $k){
+                if(isset($tmp[$k])){
+                    $tmp = $tmp[$k];
+                }else {
+                    break;
+                }
+
+            }
+            return $tmp;
+        }
+
+        return isset($data[$key]) ? $data[$key] : $data;
+    }
+
+    /**
+     * @return array
+     */
     public function getContent()
     {
-        if (is_object($this->body)) {
-            $this->body = Parse::objectToArray($this->body);
+        if(is_null($this->data)){
+            if (is_object($this->body)) {
+                $this->data = Parse::objectToArray($this->body);
+            }
+
+            if (is_string($this->body)) {
+                $this->data = Parse::jsonToArray($this->body);
+            }
         }
 
-        if (is_string($this->body)) {
-            $this->body = Parse::jsonToArray($this->body);
-        }
-
-        return $this->body;
+        return $this->data;
     }
 }
