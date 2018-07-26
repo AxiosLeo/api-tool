5274a59e6c82e00cfb48a79573865461b37d4837
axios <axioscros@aliyun.com>
2018-07-26 10:08:58 +0800

    支持静态调用

--- src/Http.php
+++ src/Http.php
@@ -9,16 +9,44 @@
 namespace api\tool;
 
 use api\tool\lib\HttpHelper;
+use api\tool\lib\HttpResponse;
 
-class Http extends HttpHelper
+/**
+ * Class Http
+ * @package api\tool
+ * @method $this setHeader($header_name, $header_content = '') static
+ * @method $this setParam($key, $value = null) static
+ * @method $this setOption($key, $value = '') static
+ * @method $this setMethod($method) static
+ * @method $this setDomain($domain) static
+ * @method $this setFormat($format) static
+ * @method HttpResponse curl($path = '', $data = []) static
+ */
+class Http
 {
     private static $instance;
 
+    /**
+     * @param array $options
+     * @return HttpHelper
+     */
     public static function instance($options = [])
     {
         if (is_null(self::$instance)) {
-            self::$instance = new static($options);
+            self::$instance = new HttpHelper($options);
         }
         return self::$instance;
+    }
+
+    /**
+     * @return void
+     */
+    public static function clear(){
+        self::$instance = null;
+    }
+
+    public static function __callStatic($name, $arguments)
+    {
+        return call_user_func_array([self::instance(),$name],$arguments);
     }
 }
