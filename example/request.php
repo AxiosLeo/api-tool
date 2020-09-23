7887de795f0315b07c9cf687f7540ab69cd2727f
axios <axiosleo@foxmail.com>
2020-09-23 21:29:10 +0800

    refactor request

--- example/request.php
+++ example/request.php
@@ -1,17 +1,8 @@
 <?php
-/**
- * @author  : axios
- * @email   : axiosleo@foxmail.com
- * @blog    : http://hanxv.cn
- * @datetime: 2018/7/3 11:52
- */
-require_once __DIR__ . '/base.php';
 
-$param = \api\tool\Request::instance()->param();
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$request = new \api\tool\Request();
+$param   = $request->param();
 
 dump($param);
-
-/*
- * More Document :
- *       https://www.kancloud.cn/manual/thinkphp5/158834
- */
